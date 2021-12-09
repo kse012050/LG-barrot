da90662fc6a2ddb76e84c33c4fa75f0eaa1b4c95
김성은 <kse012050@naver.com>
2021-12-10 08:58:57 +0900

    PC PX fin

--- js/main.js
+++ js/main.js
@@ -12,4 +12,14 @@ $(document).ready(function(){
         nextText : '<img src="images/slider_next.png" alt="slider next button">',
         pagerCustom: '.thumbPager'
     });
+
+    console.log($('.SpeciArea table tr > *'));
+    // $('.SpeciArea table tr > *:nth-child(2)').addClass('active');
+    $('.SpeciArea table').find('tr > td').eq(0).addClass('active');
+    $('.SpeciArea .tabBtn button').click(function(){
+        $('.SpeciArea .tabBtn button').removeClass('active');
+        $(this).addClass('active');
+        $('.SpeciArea table tr > *').removeClass('active');
+        $('.SpeciArea table tr > *:nth-child('+ ($(this).index() + 2) +')').addClass('active');
+    })
 });
