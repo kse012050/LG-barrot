827ea5ca8c39c5cfee83f1ca672a1885ee240357
김성은 <kse012050@naver.com>
2021-12-10 07:15:57 +0900

    function 페이지 완료했다

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.functionArea .slider').bxSlider({
+        slideMargin: 80,
+        pager:false,
+        controls:false
+    });
+
+    $('.productArea .slider').bxSlider({
+
+    });
+});
