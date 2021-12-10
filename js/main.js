78d4af46f608b784e472e04fe9701f858d132fd9
김성은 <kse012050@naver.com>
2021-12-10 11:49:27 +0900

    mobile px fin

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
     $('.functionArea .slider').bxSlider({
-        slideMargin: 80,
         pager:false,
         controls:false
-    });
+    })
+    
 
     $('.productArea .slider').bxSlider({
         prevSelector : '.prevSelector',
@@ -12,14 +12,16 @@ $(document).ready(function(){
         nextText : '<img src="images/slider_next.png" alt="slider next button">',
         pagerCustom: '.thumbPager'
     });
+    
 
-    console.log($('.SpeciArea table tr > *'));
-    // $('.SpeciArea table tr > *:nth-child(2)').addClass('active');
-    $('.SpeciArea table').find('tr > td').eq(0).addClass('active');
+    $('.SpeciArea table tr > *:nth-child(2)').addClass('active');
+    // $('.SpeciArea table').eq(0).addClass('active');
     $('.SpeciArea .tabBtn button').click(function(){
         $('.SpeciArea .tabBtn button').removeClass('active');
         $(this).addClass('active');
         $('.SpeciArea table tr > *').removeClass('active');
         $('.SpeciArea table tr > *:nth-child('+ ($(this).index() + 2) +')').addClass('active');
     })
+
+   
 });
