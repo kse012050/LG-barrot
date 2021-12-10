fd01028a4510cbfa46ddf9abbb945226e5a6cc2c
김성은 <kse012050@naver.com>
2021-12-10 12:08:17 +0900

    코드 정리 fin

--- js/main.js
+++ js/main.js
@@ -12,16 +12,12 @@ $(document).ready(function(){
         nextText : '<img src="images/slider_next.png" alt="slider next button">',
         pagerCustom: '.thumbPager'
     });
-    
 
     $('.SpeciArea table tr > *:nth-child(2)').addClass('active');
-    // $('.SpeciArea table').eq(0).addClass('active');
     $('.SpeciArea .tabBtn button').click(function(){
         $('.SpeciArea .tabBtn button').removeClass('active');
         $(this).addClass('active');
         $('.SpeciArea table tr > *').removeClass('active');
         $('.SpeciArea table tr > *:nth-child('+ ($(this).index() + 2) +')').addClass('active');
     })
-
-   
 });
